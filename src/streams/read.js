9730dd8f34c4f701671ef6acab64e190968bc50d
IlhomMax <parnasmi@gmail.com>
2025-04-28 15:48:08 +0500

    feat(streams): implement read, write, and transform stream operations

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,31 @@
+import { promises as fsPromises } from "fs";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { pipeline } from "stream/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const fileToRead = path.join(__dirname, "files", "fileToRead.txt");
+
+  try {
+    //Check if file exists
+    try {
+      await fsPromises.access(fileToRead);
+
+      //Create readable stream
+      const readStream = fs.createReadStream(fileToRead);
+
+      // Pipe to process.stdout
+      await pipeline(readStream, process.stdout);
+    } catch (error) {
+      throw new Error("FS operation failed");
+    }
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await read();
