59b02ac3232314c1ddbbbc324837d5fa6f7d7512
IlhomMax <parnasmi@gmail.com>
2025-04-28 14:17:36 +0500

    feat(cli): implement env and args parsing functionality

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+
+  const formattedArgs = [];
+
+  for (let i = 0; i < args.length; i += 2) {
+    if (args[i].startsWith("--")) {
+      const property = args[i].slice(2);
+      const value = args[i + 1];
+
+      formattedArgs.push(`${property} is ${value}`);
+    }
+  }
+
+  console.log(formattedArgs.join(", "));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envVars = process.env;
+
+  const vars = Object.keys(envVars)
+    .filter((key) => key.startsWith("RSS_"))
+    .map((key) => `${key}=${envVars[key]}`);
+
+  console.log(vars.join(";"));
 };
 
 parseEnv();
