05064f7a180c8713db3863c2bc5e1875deb28e9e
IlhomMax <parnasmi@gmail.com>
2025-04-28 19:13:23 +0500

    feat: implement worker threads for parallel Fibonacci calculations

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,40 @@
+import { Worker } from "worker_threads";
+import os from "os";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const performCalculations = async () => {
-    // Write your code here
+  const numCPUs = os.cpus().length;
+  const workers = [];
+  const results = [];
+  for (let i = 0; i < numCPUs; i++) {
+    workers.push(
+      new Promise((resolve) => {
+        const worker = new Worker(path.join(__dirname, "worker.js"));
+        worker.postMessage(10 + i);
+
+        worker.on("message", (data) => {
+          resolve({ status: "resolved", data });
+        });
+
+        worker.on("error", () => {
+          resolve({ status: "error", data: null });
+        });
+
+        worker.on("exit", (code) => {
+          if (code !== 0) {
+            resolve({ status: "error", data: null });
+          }
+        });
+      })
+    );
+  }
+
+  const allResults = await Promise.allSettled(workers);
+  console.log(allResults);
 };
 
 await performCalculations();
